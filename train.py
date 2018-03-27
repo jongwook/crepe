d941ff6f4c37168e4e4d12754ff80a7ec183b7ac
Jong Wook Kim <jongwook@nyu.edu>
2018-03-27 04:02:29 -0400

    passing all validation datasets to keras

--- train.py
+++ train.py
@@ -23,24 +23,28 @@ class PitchAccuracyCallback(keras.callbacks.Callback):
 
     def on_epoch_end(self, epoch, _):
         names = ['medleydb', 'rwcsynth', 'nsynth-test', 'nsynth-valid']
+        print(file=sys.stderr)
         for audio, true_cents in self.val_sets:
             predicted = self.model.predict(audio)
             predicted_cents = to_weighted_average_cents(predicted)
             diff = np.abs(true_cents - predicted_cents)
             mae = np.mean(diff[np.isfinite(diff)])
             rpa, rca = accuracies(true_cents, predicted_cents)
-            print("Epoch {}, Dataset {}: MAE = {}, RPA = {}, RCA = {}".format(epoch, names.pop(0), mae, rpa, rca), file=sys.stderr)
+            nans = np.mean(np.isnan(diff))
+            print("Epoch {}, Dataset {}: MAE = {}, RPA = {}, RCA = {}, nans = {}".format(epoch, names.pop(0), mae, rpa, rca, nans), file=sys.stderr)
         print(file=sys.stderr)
 
 
 def main():
     train_set, val_sets = prepare_datasets()
+    val_data = Dataset.concat([Dataset(*val_set) for val_set in val_sets]).collect()
 
     model: keras.Model = build_model()
     model.summary()
 
     model.fit_generator(iter(train_set), steps_per_epoch=options['steps_per_epoch'], epochs=options['epochs'],
-                        callbacks=get_default_callbacks() + [PitchAccuracyCallback(val_sets)])
+                        callbacks=get_default_callbacks() + [PitchAccuracyCallback(val_sets)],
+                        validation_data=val_data)
 
 
 if __name__ == "__main__":

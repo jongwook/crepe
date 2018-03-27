d56b43d87e2aaead834ef404b322c0e6b06b650e
Jong Wook Kim <jongwook@nyu.edu>
2018-03-27 03:04:07 -0400

    computing validation accuracy separately per dataset

--- train.py
+++ train.py
@@ -4,40 +4,43 @@ from datasets import *
 from evaluation import accuracies
 
 
-def prepare_data() -> (Dataset, (np.ndarray, np.ndarray)):
+def prepare_datasets() -> (Dataset, (np.ndarray, np.ndarray)):
     train = train_dataset(batch_size=options['batch_size'])
     print("Train dataset:", train, file=sys.stderr)
 
-    print("Collecting validation set:", file=sys.stderr)
-    validation = validation_dataset(seed=42, take=100).take(4000).collect(verbose=True)
-    print("Validation data:", validation[0].shape, validation[1].shape, file=sys.stderr)
+    validation = []
+    for name in ['medleydb', 'rwcsynth', 'nsynth-test', 'nsynth-valid']:
+        print("Collecting validation set {}:".format(name), file=sys.stderr)
+        dataset = validation_dataset(name, seed=42, take=100).take(2000).collect(verbose=True)
+        validation.append(dataset)
 
     return train, validation
 
 
 class PitchAccuracyCallback(keras.callbacks.Callback):
-    def __init__(self, val_data):
-        self.audio = val_data[0]
-        self.true_cents = to_weighted_average_cents(val_data[1])
+    def __init__(self, val_sets):
+        self.val_sets = [(audio, to_weighted_average_cents(pitch)) for audio, pitch in val_sets]
 
     def on_epoch_end(self, epoch, _):
-        predicted = self.model.predict(self.audio, verbose=1)
-        predicted_cents = to_weighted_average_cents(predicted)
-        diff = np.abs(self.true_cents - predicted_cents)
-        mae = np.mean(diff[np.isfinite(diff)])
-        rpa, rca = accuracies(self.true_cents, predicted_cents)
-        print("\nEpoch {}: MAE = {}, RPA = {}, RCA = {}\n".format(epoch, mae, rpa, rca), file=sys.stderr)
+        names = ['medleydb', 'rwcsynth', 'nsynth-test', 'nsynth-valid']
+        for audio, true_cents in self.val_sets:
+            predicted = self.model.predict(audio)
+            predicted_cents = to_weighted_average_cents(predicted)
+            diff = np.abs(true_cents - predicted_cents)
+            mae = np.mean(diff[np.isfinite(diff)])
+            rpa, rca = accuracies(true_cents, predicted_cents)
+            print("Epoch {}, Dataset {}: MAE = {}, RPA = {}, RCA = {}".format(epoch, names.pop(0), mae, rpa, rca), file=sys.stderr)
+        print(file=sys.stderr)
 
 
 def main():
-    train_data, val_data = prepare_data()
+    train_set, val_sets = prepare_datasets()
 
     model: keras.Model = build_model()
     model.summary()
 
-    model.fit_generator(iter(train_data), steps_per_epoch=options['steps_per_epoch'], epochs=options['epochs'],
-                        callbacks=get_default_callbacks() + [PitchAccuracyCallback(val_data)],
-                        validation_data=val_data)
+    model.fit_generator(iter(train_set), steps_per_epoch=options['steps_per_epoch'], epochs=options['epochs'],
+                        callbacks=get_default_callbacks() + [PitchAccuracyCallback(val_sets)])
 
 
 if __name__ == "__main__":
